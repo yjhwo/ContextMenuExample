5f98e2fd98489f9fb1ae6097be52adcf99fb20f2
yjhwo <yjhwo@naver.com>
2016-05-26 15:31:42 +0900

    0526_수업완료

--- app/src/main/java/com/estsoft/contextmenuexample/MainActivity.java
+++ app/src/main/java/com/estsoft/contextmenuexample/MainActivity.java
@@ -3,7 +3,9 @@ package com.estsoft.contextmenuexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 컨텍스트 메뉴를 등록할 View를 설정 ( Root View )
+        this.registerForContextMenu( findViewById(android.R.id.content) );
+                                                    // activity의 content view
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        switch ( v.getId() ){
+            case android.R.id.content:
+                getMenuInflater().inflate( R.menu.main_menu, menu);
+                break;
+            default:
+                break;
+        }
+
+        //super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch ( item.getItemId() ){
+            case R.id.menu_refresh:
+                Toast.makeText(this, "refresh", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_search:
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
